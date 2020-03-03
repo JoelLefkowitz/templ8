8cfe1818b1ab1eb04ebf6f4f5dd4868f25585371
JoelLefkowitz <joellefkowitz@hotmail.com>
2020-03-03 13:13:24 +0000

    Bump version: 0.3.3 → 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 from setuptools import find_packages, setup
 from distutils.core import Command
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 
 with open("README.md", "r") as f:
     long_description = f.read()
